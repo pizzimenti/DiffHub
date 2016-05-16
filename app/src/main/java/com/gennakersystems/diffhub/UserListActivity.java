414da4676d8eb70dcd69b7f7ca9a2650c0bed20c
Bradley Pizzimenti <brad@gennakersystems.com>
2016-05-16 15:45:54 -0700

    query user object from GitHub and log it within UserListActivity

--- app/src/main/java/com/gennakersystems/diffhub/UserListActivity.java
+++ app/src/main/java/com/gennakersystems/diffhub/UserListActivity.java
@@ -3,15 +3,22 @@ package com.gennakersystems.diffhub;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class UserListActivity extends AppCompatActivity {
+    public static final String TAG = UserListActivity.class.getSimpleName();
 
-
-    @BindView(R.id.usernameTextView) TextView mUsernameTextView;
+    @BindView(R.id.usernameTextView)
+    TextView mUsernameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,28 @@ public class UserListActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String username = intent.getStringExtra("username");
         mUsernameTextView.setText("Repositories for the user: " + username);
+        getUser(username);
+
+    }
+
+    private void getUser(String username) {
+        ghService.findUser(username, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String jsonData = response.body().string();
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, jsonData);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }

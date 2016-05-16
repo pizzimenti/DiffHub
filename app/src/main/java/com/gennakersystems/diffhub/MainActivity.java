8234dfe61a8d4e3235228cd3794f84391fc91a5e
Brad Pizzimenti <brad@gennakersystems.com>
2016-05-15 20:21:22 -0700

    create onClickListener for button and test

--- app/src/main/java/com/gennakersystems/diffhub/MainActivity.java
+++ app/src/main/java/com/gennakersystems/diffhub/MainActivity.java
@@ -2,12 +2,25 @@ package com.gennakersystems.diffhub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mFindUserButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindUserButton = (Button) findViewById(R.id.findUserButton);
+        mFindUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hello from DiffHub.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

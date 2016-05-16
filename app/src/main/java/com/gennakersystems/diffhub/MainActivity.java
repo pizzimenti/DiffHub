683f9b9eaea499c07322cc9138190cceff45796e
Brad Pizzimenti <brad@gennakersystems.com>
2016-05-15 22:12:55 -0700

    pass username to UserListActivity

--- app/src/main/java/com/gennakersystems/diffhub/MainActivity.java
+++ app/src/main/java/com/gennakersystems/diffhub/MainActivity.java
@@ -3,24 +3,31 @@ package com.gennakersystems.diffhub;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     private Button mFindUserButton;
+    private EditText mUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mUserName = (EditText) findViewById(R.id.usernameEditText);
         mFindUserButton = (Button) findViewById(R.id.findUserButton);
         mFindUserButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String username = mUserName.getText().toString();
                 Intent intent = new Intent(MainActivity.this, UserListActivity.class);
+                intent.putExtra("username", username);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/gennakersystems/diffhub/UserListActivity.java
+++ app/src/main/java/com/gennakersystems/diffhub/UserListActivity.java
@@ -1,13 +1,23 @@
 package com.gennakersystems.diffhub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class UserListActivity extends AppCompatActivity {
+
+    private TextView mUsernameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
+
+        mUsernameTextView = (TextView) findViewById(R.id.usernameTextView);
+
+        Intent intent = getIntent();
+        String username = intent.getStringExtra("username");
+        mUsernameTextView.setText("Repositories for the user: " + username);
     }
 }

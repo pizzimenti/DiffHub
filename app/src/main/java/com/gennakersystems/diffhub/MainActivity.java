27ae2079fb180ecbc5695c23f1528e00271db306
Brad Pizzimenti <brad@gennakersystems.com>
2016-05-15 21:00:24 -0700

    intend to a new activity (UserListActivity)

--- app/src/main/java/com/gennakersystems/diffhub/MainActivity.java
+++ app/src/main/java/com/gennakersystems/diffhub/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gennakersystems.diffhub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         mFindUserButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Hello from DiffHub.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, UserListActivity.class);
+                startActivity(intent);
             }
         });
     }

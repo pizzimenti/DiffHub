7b16a03cced5ba13aa6a95317a0f5c92ff5622c5
Brad Pizzimenti <brad@gennakersystems.com>
2016-05-16 23:31:42 -0700

    model incoming user object data

---
+++ app/src/main/java/com/gennakersystems/diffhub/User.java
@@ -0,0 +1,57 @@
+package com.gennakersystems.diffhub;
+
+import java.util.Date;
+
+public class User {
+    private String mUsername;
+    private String mName;
+    private String mImageUrl;
+    private String mEmail;
+    private Date mCreationDate;
+    private int mRepos;
+    private int mFollowers;
+    private int mFollowing;
+
+    public User(String username, String name, String imageUrl, String email, Date creationDate, int repos, int followers, int following) {
+        this.mUsername = username;
+        this.mName = name;
+        this.mImageUrl = imageUrl;
+        this.mEmail = email;
+        this.mCreationDate = creationDate;
+        this.mRepos = repos;
+        this.mFollowers = followers;
+        this.mFollowing = following;
+    }
+
+    public String getUsername() {
+        return mUsername;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getImageUrl() {
+        return mImageUrl;
+    }
+
+    public String getEmail() {
+        return mEmail;
+    }
+
+    public Date getCreationDate() {
+        return mCreationDate;
+    }
+
+    public int getRepos() {
+        return mRepos;
+    }
+
+    public int getFollowers() {
+        return mFollowers;
+    }
+
+    public int getFollowing() {
+        return mFollowing;
+    }
+}
